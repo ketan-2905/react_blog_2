4bb8f1e4e2c44ef6743b6b00d856c3ef917a5c61
ketan-2905 <159174685+ketan-2905@users.noreply.github.com>
2024-10-13 13:00:25 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import Feed from '../Feed'
 const Home = ({posts}) => {
   return (
     <main className='Home'>
-      {posts.length ? <Feed posts={posts}/> : <p style={{'margin-top':'2rem', 'font-weight':'800'}}>There is no post more</p>}
+      {posts.length ? <Feed posts={posts}/> : <p style={{'margin-top':'2rem', 'font-weight':'800'}}>There is no post more add post in Post section</p>}
     </main>
   )
 }
